a1b18457b5bca8636dddb38501d6e8fada35b438
Kgharte <ghartekalpesh10@gmail.com>
2019-07-13 14:18:20 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from "@angular/flex-layout";
+//Material Section
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import  {MatButtonModule } from '@angular/material';
+import { MatCheckboxModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
+import { MatSidenavModule, MatToolbarModule } from '@angular/material';
+import { MatMenuModule} from '@angular/material/menu';
+import { HeaderComponent } from './components/header/header.component';
+
 
 @NgModule({
+
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatMenuModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
