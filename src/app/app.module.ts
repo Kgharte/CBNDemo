5247859a5fb6069a5b673255adc68ceda926b8ef
Anoopkumar <aanoopkumars@gmail.com>
2019-07-13 16:28:36 +0530

    material modules added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FlexLayoutModule } from "@angular/flex-layout";
-//Material Section
+import { FlexLayoutModule } from '@angular/flex-layout';
+// Material Section
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import  {MatButtonModule } from '@angular/material';
+import {MatButtonModule, MatInputModule } from '@angular/material';
 import { MatCheckboxModule } from '@angular/material';
 import { MatIconModule } from '@angular/material';
 import { MatSidenavModule, MatToolbarModule } from '@angular/material';
 import { MatMenuModule} from '@angular/material/menu';
 import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './components/login/login.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -25,13 +28,17 @@ import { LoginComponent } from './components/login/login.component';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MatButtonModule, 
+    MatButtonModule,
     MatCheckboxModule,
     MatIconModule,
     MatSidenavModule,
     MatToolbarModule,
     MatMenuModule,
-    FlexLayoutModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FlexLayoutModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
